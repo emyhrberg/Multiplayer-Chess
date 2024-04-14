1c7cd5c9f3e9b90167386be622a85f90f8d2586d
emyhrberg <erik.myhrberg@hotmail.com>
2024-04-14 19:20:44 +0200

    Added functionality for handling disconnects and client refreshing should assign the right color

--- server.js
+++ server.js
@@ -8,17 +8,21 @@ const socket = require('socket.io')(server);
 app.use(express.static(__dirname + '/public'));
 
 // Keep track of the players
-let playerCount = 0;
+var playerCount = 0;
+var white = {id: null, time: 30};
+var black = {id: null, time: 30};
 
 // Handle players joining the game
 socket.on('connection', (socket) => {
+    playerCount += 1;
 
-    // Assign the player a color
-    playerCount++;
-    if (playerCount === 1) {
+    // Assign the current player a color
+    if (playerCount === 1 && !white.id) {
+        white.id = socket.id;
         socket.emit('playerJoined', 'white');
         console.log('white joined')
-    } else {
+    } else if (playerCount === 2 && !black.id) {
+        black.id = socket.id;
         socket.emit('playerJoined', 'black');
         console.log('black joined')
     }
@@ -27,9 +31,25 @@ socket.on('connection', (socket) => {
     // Receives a move from the player and broadcast to the other player
     socket.on('move', (move) => {
         socket.broadcast.emit('move', move);
-        console.log('[server.js] ' + move.color, ' move: ', move.san)
+        // console.log(move.color, ' move: ', move.san)
     });
+
+    // Handle player disconnect
+    // Give the first player white and the second player black
+    socket.on('disconnect', () => {
+        playerCount -= 1;
+        if (socket.id === white.id) {
+            white.id = null;
+            console.log('white disconnected')
+        }
+        if (socket.id === black.id) {
+            black.id = null;
+            console.log('black disconnected')
+        }
+    });
+
 });
+
 
 // Start the server on port 4000
 const PORT = 4000;
